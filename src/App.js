c5e8df3af6f8b9c7ed1dd51f987a178c2af81a7a
Melissa Morgan <melissamorgan@Melissas-MacBook-Pro.local>
2017-06-27 01:28:14 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -13,15 +13,25 @@ class App extends Component {
       <div className="App">
       
         <img src={gif} className="lbgif" alt="gif" />
-        <p className="App-intro">
-When an evil scientist named Dr. Con creates evil clones from programmable metal and human DNA, the clones destroy 75% of humanity and enslave the other 25% in cities all over the world. Before being captured, a team of computer programmers and engineers from New York City create an intelligent robot to help free humanity from the evil clones and to destroy Dr. Con. The Last Bot is humanity’s only hope of freedom and survival!
+      
+        {/* <p className="App-intro">
+ When an evil scientist named Dr. Con creates evil clones from programmable metal and human DNA, the clones destroy 75% of humanity and enslave the other 25% in cities all over the world. Before being captured, a team of computer programmers and engineers from New York City create an intelligent robot to help free humanity from the evil clones and to destroy Dr. Con. The Last Bot is humanity’s only hope of freedom and survival!
 </p>
 <p>Game Objectives: Destroy the clones with your acid gun, free the humans in each level, find Dr. Con’s hideouts and destroy cloning centers. The final level is to destroy Dr. Con.
-        </p>
-        <button type="submit" className="playbutton">Submit</button>
-        <p><a href ="#" onClick={alert("Hello")}>Click for more info</a></p>
+        </p>*/}
+
+           {/* <form className="formone">
+          <label>
+            <p>Create Username:</p>
+            <input type="text" name="name" /> <br /><br />
+          </label>
+          <input type="submit" value="PLAY" className="playbutton"/>
+        </form>
+       {/*  <button type="submit" className="playbutton"><h2>Play</h2></button>*/}
+        {/* <p><a href ="#" onClick={alert("Hello")}>Click for more info</a></p>*/}
       </div>
-    );
+);
+
   }
 }
 
